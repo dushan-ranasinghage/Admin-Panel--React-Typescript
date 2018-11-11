4cec6ef3dfeb495e373b4861462c9ea27f2a8713
Dushan Randika <DushanR@millenniumitesp.com>
2018-11-11 19:43:58 +0530

    Deleted bootstrap Locally and tested a functional component

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,17 +1,30 @@
 import * as React from 'react';
 
+interface IWelcomeProps {
+  name: string,
+ }
+
+ const Welcome: React.SFC<IWelcomeProps> = (props) => {
+  return <h1>Hello, {props.name}</h1>;
+ }
+
 class Test extends React.Component {
 
   constructor(props: any){
-    super(props);
-}
+      super(props);
+  }
+
   public render() {
     return (
       <div>
-      Test
+      
+      <button type="button" className="btn btn-default">button</button>
+      <Welcome name={'Dushan Randika'}/>
       </div>
     );
   }
 }
 
 export default Test;
+
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import './assets/css/index.css';
 import Test from './components/Test'
 import registerServiceWorker from './registerServiceWorker';
 
