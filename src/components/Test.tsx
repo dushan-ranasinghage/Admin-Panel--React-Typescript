c1f553b975a096273ce384f2d752157add6cbe46
Dushan Randika <DushanR@millenniumitesp.com>
2018-11-11 19:07:58 +0530

    Created a simple skeleton of the project

---
+++ src/components/Test.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+class Test extends React.Component {
+
+  constructor(props: any){
+    super(props);
+}
+  public render() {
+    return (
+      <div>
+      Test
+      </div>
+    );
+  }
+}
+
+export default Test;
