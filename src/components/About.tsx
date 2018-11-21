9fe414c01c04b3ac1b127e42088a5581e75ebec7
Dushan Randika <DushanR@millenniumitesp.com>
2018-11-21 07:54:28 +0530

    Routes Added

---
+++ src/components/About.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+class About extends React.Component {
+    public render() {
+      return (
+        <div>
+        
+        About
+        </div>
+      );
+    }
+  }
+  
+  export default About;
+  
+  
+  
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import Test from './components/Test'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import About from './components/About';
+import Test from './components/Test';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <div>
-  <Test />
-  </div>,
+  <Router>
+    <div>
+    <Route exact={true} path="/" component={Test} />
+  <Route path="/about" component={About} />
+    </div>
+  </Router>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
